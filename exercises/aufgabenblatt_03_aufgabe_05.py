db79273e9bc3c23e7864ff58874b9fca4a060b6f
Yannick93qw <hutteryannick@gmail.com>
2023-05-21 10:26:25 +0200

    Solve part b of exercise 5

--- exercises/aufgabenblatt_03_aufgabe_05.py
+++ exercises/aufgabenblatt_03_aufgabe_05.py
@@ -14,24 +14,24 @@ df = pd.DataFrame({'y': np.random.normal(loc=0, scale=10, size=1000),
                    'x': np.random.normal(loc=10, scale=2, size=1000)})
 
 app.layout = html.Div([html.H1("Dashboard 2"),
-                       dbc.Row([dbc.Col([dcc.Dropdown(options=['red', 'green', 'blue'], value='red', id='color', multi=False)], width=6),
-                                dbc.Col([dcc.RangeSlider(min=math.floor(df['y'].min()), max=math.ceil(df['y'].max()), allowCross=False , id="range_slider")
-                                         ], width=6)
+                       dbc.Row([dbc.Col([dcc.RangeSlider(min=math.floor(df['y'].min()), max=math.ceil(df['y'].max()), allowCross=False , id="range_slider")
+                                         ], width=6),
+                                dbc.Col([dcc.Dropdown(options=['red', 'green', 'blue'], value='red', id='color', multi=False)], width=6),
                                 ]),
-                       dbc.Row([dbc.Col([dcc.Graph(id="graph_1")], width=6),
-                                dbc.Col([dcc.Graph(id="graph_2")], width=6)
+                       dbc.Row([dbc.Col([dcc.Graph(id="histogram")], width=6),
+                                dbc.Col([dcc.Graph(id="scatter")], width=6)
                                 ])], className="m-4")
 
-@app.callback(Output("graph_1", "figure"), Input("color", "value"))
 
-def update_graph_1(dropdown_value_color):
-    fig = px.histogram(df, x="y", color_discrete_sequence=[dropdown_value_color])
+@app.callback(Output("scatter", "figure"), Input("color", "value"))
+def update_scatter(dropdown_value_color):
+    fig = px.scatter(df, x="y", color_discrete_sequence=[dropdown_value_color])
     fig.update_layout()
     return fig
 
-@app.callback(Output("graph_2", "figure"), Input("range_slider", "value"))
 
-def update_graph_2(range_slider):
+@app.callback(Output("histogram", "figure"), Input("range_slider", "value"))
+def update_histogram(range_slider):
     dff = df.copy()
     # Check if we actually have a value and we have our two expected values (length of 2 for min and max.)
     if range_slider is not None and len(range_slider) == 2:
@@ -39,7 +39,7 @@ def update_graph_2(range_slider):
         max_value = range_slider[1]
         # Filter by min and max value
         dff = df[(df['y'] >= min_value) & (df['y'] <= max_value)]
-    fig = px.scatter(dff, x='x', y='y')
+    fig = px.histogram(dff, x='x', y='y')
     fig.update_layout()
     return fig
 
