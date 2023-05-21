4ccdab93bba8c5fc46a24ae4f508bbeba3d365f8
Yannick93qw <hutteryannick@gmail.com>
2023-05-21 15:34:42 +0200

    Solve part b of exercise 6

--- exercises/aufgabenblatt_03/aufgabenblatt_03_aufgabe_06.py
+++ exercises/aufgabenblatt_03/aufgabenblatt_03_aufgabe_06.py
@@ -7,10 +7,6 @@ import dash_bootstrap_components as dbc
 
 # new: plotly template="plotly_white", https://plotly.com/python/templates/
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-
-df = pd.DataFrame({'y': np.random.normal(loc=0, scale=10, size=1000), 'x': np.random.normal(loc=10, scale=2, size=1000)})
-
 """
 Generates some sample data for a country. Samples is the number of data which should be generated.
 Currently it creates a random happiness value per day.
@@ -19,11 +15,22 @@ def generate_country_data(samples):
     dates = pd.date_range('2023-05-21', periods=samples, freq='1D')
     return pd.DataFrame({'date': dates, 'happiness': np.random.normal(loc=10, scale=10, size=samples)})
 
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+df = pd.DataFrame({'y': np.random.normal(loc=0, scale=10, size=1000), 'x': np.random.normal(loc=10, scale=2, size=1000)})
+
 # The name of the countries which can be selected in the dropdown on tab 2.
 countries = ["Switzerland", "Sweden"]
 
 # Create dictionary where the keys are the name of the countries. This makes it easier to get out the specific data for a country.
 df_countries = {country_name: generate_country_data(30) for country_name in countries}
+
+
+# Read in real data...
+df_real_data = pd.read_csv("./data.csv", encoding="utf-8")
+df_real_data_grouped_by_country = df_real_data.groupby("Country Name")
+real_countries = list(df_real_data["Country Name"].unique())
 
 app.layout = html.Div([html.Div([html.H1("Dashboard 3")], className="header"), html.Div([dcc.Tabs(id="tabs", children=[
     dcc.Tab(label='Tab One', id="tab_1_graphs", children=[html.Div([
@@ -38,7 +45,17 @@ app.layout = html.Div([html.Div([html.H1("Dashboard 3")], className="header"), h
             dbc.Row([
                 dbc.Col([dcc.Graph(id="tab_2_bar")], width=6),
                 dbc.Col([html.H3(id="tab_2_avg_happiness")], width=6), ])
-            ],className="tab_content")]),])], className="content")])
+            ],className="tab_content")]),
+
+        dcc.Tab(label='Tab Three', id="tab_3_graphs", children=[html.Div([
+            dbc.Row([
+                dbc.Col([dcc.Dropdown(options=real_countries, value=real_countries[0], id='real_countries', multi=False)], width=6), 
+                dbc.Col([html.H1("Average Life Ladder")], width=6)]),
+            dbc.Row([
+                dbc.Col([dcc.Graph(id="tab_3_bar")], width=6),
+                dbc.Col([html.H3(id="tab_3_avg_life_ladder")], width=6), ])
+            ],className="tab_content")]),
+        ])], className="content")])
 
 
 # Tab 1
@@ -60,6 +77,22 @@ def update_tab_2_avg_happiness(dropdown_countries_value):
     df_country = df_countries[dropdown_countries_value]
     avg = df_country["happiness"].mean()
     return f"{dropdown_countries_value} has an average happiness of {avg}"
+
+# Tab 3 
+@app.callback(Output("tab_3_bar", "figure"), Input("real_countries", "value"))
+def update_tab_3_bar(dropdown_countries_value):
+    df_real_country = df_real_data_grouped_by_country.get_group(dropdown_countries_value)
+    print(df_real_country)
+    fig = px.bar(df_real_country, x="Year", y="Life Ladder")
+    fig.update_layout(template="plotly_white")
+    return fig
+
+@app.callback(Output("tab_3_avg_life_ladder", "children"), Input("real_countries", "value"))
+def update_tab_3_avg_life_ladder(dropdown_countries_value):
+    df_real_country = df_real_data_grouped_by_country.get_group(dropdown_countries_value)
+    avg = df_real_country["Life Ladder"].mean()
+    return f"{dropdown_countries_value} has an average Life Ladder of {avg}"
+
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=8000)
